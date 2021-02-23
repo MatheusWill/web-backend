f530c4e782549362d83f2a701ac1ac96a18b3745
MATHEUS WILLIAN DE OLIVEIRA <20272574@cfp127aluno.local>
2021-02-23 11:28:56 -0300

    Aula completa

---
+++ php-introducao/form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="GET" action="variaveis-pre-definidas.php">
+        <input type="text" name="nome" placeholder="Digite seu nome..">
+
+        <input type="number" name="idade" placeholder="Digite a sua idade..">
+
+        <button>Enviar</button>
+    </form> 
+</body>
+</html>
