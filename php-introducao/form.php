735255dd70d7362389d076b7673d7d806c902938
Matheus Willian de Oliveira <matheuswill6663@gmail.com>
2021-02-25 08:31:19 -0300

    trocando a branch

--- php-introducao/form.php
+++ php-introducao/form.php
@@ -7,9 +7,9 @@
 </head>
 <body>
     <form method="GET" action="variaveis-pre-definidas.php">
-        <input type="text" name="nome" placeholder="Digite seu nome..">
+        <input type="text" name="nome" placeholder="Digite seu nome.">
 
-        <input type="number" name="idade" placeholder="Digite a sua idade..">
+        <input type="number" name="idade" placeholder="Digite a sua idade.">
 
         <button>Enviar</button>
     </form> 
